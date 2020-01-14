7a84bafa0244b194e06e49ae016d1ab80dcca7ae
Chris Colvard <cjcolvar@indiana.edu>
2020-01-14 11:33:19 -0500

    Rework authentication; run in cypress docker container

--- spec/cypress/support/commands.js
+++ spec/cypress/support/commands.js
@@ -10,7 +10,33 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add("login", (role) => {
+  const email = Cypress.env('USERS_' + role.toUpperCase() + '_EMAIL')
+  const password = Cypress.env('USERS_' + role.toUpperCase() + '_PASSWORD')
+
+  cy.request('/users/sign_in')
+  .its('body')
+  .then((body) => {
+    // we can use Cypress.$ to parse the string body
+    // thus enabling us to query into it easily
+    const $html = Cypress.$(body)
+    const csrfToken = $html.find('input[name=authenticity_token]').val()
+
+    cy.request({
+      method: 'POST',
+      url: '/users/sign_in',
+      body: {
+        user: {
+          login: email,
+          password: password,
+        },
+        authenticity_token: csrfToken,
+      }
+    }).then((resp) => {
+      expect(resp.status).to.eq(200)
+    })
+  })
+})
 //
 //
 // -- This is a child command --
