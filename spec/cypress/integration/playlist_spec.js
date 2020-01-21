2218ab09981ede31c5d1deb3aa771f786390da28
Chris Colvard <cjcolvar@indiana.edu>
2020-01-21 14:51:04 -0500

    Use localhost:3000 to access avalon; click button to submit playlist form to ensure JS on page executes
    There were a couple issues fixed:
    Switching to cy.get(form).submit() created bad playlists which was causing the first test to fail.  (https://github.com/avalonmediasystem/avalon/issues/3953)  I had to manually delete those bad playlists to fix the first test.
    Once the new playlist form was submitted, Avalon was redirecting to http://localhost:3000 instead of http://avalon:3000 which is what caused the cross origin error we were seeing.  I made a change to the docker and cypress config to use localhost:3000 instead of avalon:3000
    I also made two other changes:
    I reformatted the playlist_spec.js file.
    I tried to reduce the number of page requests by going straight to the page being tested instead of navigating from the home page.

--- spec/cypress/integration/playlist_spec.js
+++ spec/cypress/integration/playlist_spec.js
@@ -1,208 +1,187 @@
 context('checks navigation after logging in', () => {
 
-  //checks navigation when create new playlist is accessed
-  it('.create_playlists()', () => {
+	//checks navigation when create new playlist is accessed
+	it('.create_playlists()', () => {
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
-			//
-	cy.url().should('include', '/playlists/new')
-  })
-
-  //is able to create private (default) playlist
-  it('.validate_create_playlist()', () => {
-		//cy.login('administrator')
-			cy.visit('/')
-		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/playlists')
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-		
-	cy.get('#playlist_title').type('Cypress Testing')
-	cy.get('#playlist_comment').type('Cypress testing comments')
-	//cy.contains('Create').click()
-	//cy.get('input[name=commit]').last().click()
+		cy.url().should('include', '/playlists/new')
+	})
+
+	//is able to create private (default) playlist
+	it('.validate_create_playlist()', () => {
+		cy.login('administrator')
+		cy.visit('/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing')
+		cy.get('#playlist_comment').type('Cypress testing comments')
+		cy.get('#submit-playlist-form').click()
+		//
+		cy.visit('/playlists')
+		cy.contains('Cypress Testing')
+		cy.contains('Visibility')
+		cy.contains('Created')
+		cy.contains('Updated')
+		cy.contains('Actions')
+		cy.contains('Private')
+		cy.contains('Size')
+		cy.contains('Delete')
+		cy.contains('Edit')
+		cy.contains('Delete')
+	})
+
+	//is able to view playlist by clicking on playlist name
+	it('.view_playlist()', () => {
+		//cy.login('administrator')
+		cy.visit('/')
+		cy.login('administrator')
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
+		//
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing2')
+		cy.get('#playlist_comment').type('Cypress testing2 comments')
+		//cy.contains('Create').click()
 		cy.get('#playlist_form').submit()
-	
-	cy.visit('/playlists')
-	cy.contains('Cypress Testing')
-	cy.contains('Visibility')
-	cy.contains('Created')
-	cy.contains('Updated')
-	cy.contains('Actions')
-	cy.contains('Private')
-	cy.contains('Size')
-	cy.contains('Delete')
-	cy.contains('Edit')
-	cy.contains('Delete')
-	
-  })
 
-  //is able to view playlist by clicking on playlist name
-  it('.view_playlist()', () => {
+		cy.visit('/playlists')
+		cy.contains('Cypress Testing2').click()
+		cy.contains('Edit Playlist')
+		cy.contains('Cypress testing2 comments')
+		cy.contains('This playlist currently has no playable items')
+
+	})
+
+	//deletes playlist permanently from playlists page
+	it('.delete_playlist()', () => {
 		//cy.login('administrator')
-			cy.visit('/')
+		cy.visit('/')
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing2')
-	cy.get('#playlist_comment').type('Cypress testing2 comments')
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing3')
+		cy.get('#playlist_comment').type('Cypress testing3 comments')
+		//cy.contains('Create').click()
+		cy.get('#playlist_form').submit()
 
-	cy.visit('/playlists')
-	cy.contains('Cypress Testing2').click()
-	cy.contains('Edit Playlist')
-	cy.contains('Cypress testing2 comments')
-	cy.contains('This playlist currently has no playable items')
-	
-  })
+		cy.visit('/playlists')
+		cy.contains('Delete').click()
+		cy.contains('Yes, Delete').click()
+		//cy.contains('Playlist was successfully destroyed')
+		cy.visit('/playlists')
+		cy.contains('Cypress Testing3').should('not.exist')
+	})
 
-  //deletes playlist permanently from playlists page
-  it('.delete_playlist()', () => {
+	//is able to delete playlist from edit playlist page
+	it('.delete_playlist_edit_page()', () => {
 		//cy.login('administrator')
-			cy.visit('/')
+		cy.visit('/')
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing3')
-	cy.get('#playlist_comment').type('Cypress testing3 comments')
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing4')
+		cy.get('#playlist_comment').type('Cypress testing4 comments')
+		//cy.contains('Create').click()
+		cy.get('#playlist_form').submit()
 
-	cy.visit('/playlists')
-	cy.contains('Delete').click()
-	cy.contains('Yes, Delete').click()
-	//cy.contains('Playlist was successfully destroyed')
-	cy.visit('/playlists')
-	cy.contains('Cypress Testing3').should('not.exist')	
-  })
+		cy.visit('/playlists')
+		cy.contains('Cypress Testing4').click()
+		//cy.contains('Delete')
+		cy.contains('Edit Playlist').click()
 
+		cy.contains('Delete Playlist').click()
+		cy.contains('Yes, Delete').click()
+		cy.contains('Playlist was successfully destroyed.')
+		cy.visit('/playlists')
+		cy.contains('Cypress Testing4').should('not.exist')
+	})
 
-  //is able to delete playlist from edit playlist page
-  it('.delete_playlist_edit_page()', () => {
+	//is able to create public playlist
+	it('.create_public_playlist()', () => {
 		//cy.login('administrator')
-			cy.visit('/')
+		cy.visit('/')
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing4')
-	cy.get('#playlist_comment').type('Cypress testing4 comments')
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing5')
+		cy.get('#playlist_comment').type('Cypress testing5 comments')
+		cy.contains('Public').click()
 
-	cy.visit('/playlists')
-	cy.contains('Cypress Testing4').click()
-	//cy.contains('Delete')
-	cy.contains('Edit Playlist').click()
+		//cy.contains('Create').click()
+		cy.get('#playlist_form').submit()
 
-	cy.contains('Delete Playlist').click()
-	cy.contains('Yes, Delete').click()
-	cy.contains('Playlist was successfully destroyed.')
-	cy.visit('/playlists')
-	cy.contains('Cypress Testing4').should('not.exist')
-	
-  })
+		cy.visit('/playlists')
 
-  //is able to create public playlist
-  it('.create_public_playlist()', () => {
+		cy.contains('Cypress Testing5')
+		cy.contains('Public')
+	})
+
+	//is able to edit playlist name and description
+	it('.edit_playlist_name()', () => {
 		//cy.login('administrator')
-			cy.visit('/')
+		cy.visit('/')
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing5')
-	cy.get('#playlist_comment').type('Cypress testing5 comments')
-	cy.contains('Public').click()
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing5')
+		cy.get('#playlist_comment').type('Cypress testing5 comments')
+		cy.contains('Public').click()
+		//cy.contains('Create').click()
+		cy.get('#playlist_form').submit()
 
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
+		cy.visit('/playlists')
 
-	cy.visit('/playlists')
+		cy.contains('Cypress Testing5').click()
+		cy.contains('Edit Playlist').click()
+		//cy.contains('Edit').click()
 
-	cy.contains('Cypress Testing5')
-	cy.contains('Public')
-	
-  })
+		cy.get('#playlist_edit_button').click()
+		cy.get('#playlist_title').type('Cypress Testing5 changed')
+		cy.get('#playlist_comment').type('Cypress testing5 comments changed')
+		cy.contains('Save Changes').click()
+		cy.contains('Playlist was successfully updated')
+	})
 
-
-  //is able to edit playlist name and description
-  it('.edit_playlist_name()', () => {
-		//cy.login('administrator')
-			cy.visit('/')
-		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
-		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing5')
-	cy.get('#playlist_comment').type('Cypress testing5 comments')
-	cy.contains('Public').click()
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
-
-
-	cy.visit('/playlists')
-
-	cy.contains('Cypress Testing5').click()
-	cy.contains('Edit Playlist').click()
-	//cy.contains('Edit').click()
-
-	cy.get('#playlist_edit_button').click()
-	cy.get('#playlist_title').type('Cypress Testing5 changed')
-	cy.get('#playlist_comment').type('Cypress testing5 comments changed')
-	cy.contains('Save Changes').click()
-	cy.contains('Playlist was successfully updated')
-	
-  })
-
-
-  //is able to change public playlist to private
-  it('.edit_access_control()', () => {
+	//is able to change public playlist to private
+	it('.edit_access_control()', () => {
 		//cy.login('administrator')
 		cy.login('administrator')
-			cy.visit('/')
-	cy.contains('Playlists').click()
-	cy.contains('Create New Playlist').click()
+		cy.visit('/')
+		cy.contains('Playlists').click()
+		cy.contains('Create New Playlist').click()
 		//
-	cy.url().should('include', '/playlists/new')
-	cy.get('#playlist_title').type('Cypress Testing5')
-	cy.get('#playlist_comment').type('Cypress testing5 comments')
-	cy.contains('Public').click()
-	//cy.contains('Create').click()
-			cy.get('#playlist_form').submit()
+		cy.url().should('include', '/playlists/new')
+		cy.get('#playlist_title').type('Cypress Testing5')
+		cy.get('#playlist_comment').type('Cypress testing5 comments')
+		cy.contains('Public').click()
+		//cy.contains('Create').click()
+		cy.get('#playlist_form').submit()
 
+		cy.visit('/playlists')
 
-	cy.visit('/playlists')
+		cy.contains('Cypress Testing5').click()
+		cy.contains('Edit Playlist').click()
+		//cy.contains('Edit').click()
 
-	cy.contains('Cypress Testing5').click()
-	cy.contains('Edit Playlist').click()
-	//cy.contains('Edit').click()
-
-	cy.get('#playlist_edit_button').click()
-	//cy.get('#playlist_title').type('Cypress Testing5 changed')
-	//cy.get('#playlist_comment').type('Cypress testing5 comments changed')
-	cy.contains('Private').click()
-	cy.contains('Save Changes').click()
-	cy.contains('Playlist was successfully updated')
-	cy.contains('Private')
-	
-  })
-
-  })
+		cy.get('#playlist_edit_button').click()
+		//cy.get('#playlist_title').type('Cypress Testing5 changed')
+		//cy.get('#playlist_comment').type('Cypress testing5 comments changed')
+		cy.contains('Private').click()
+		cy.contains('Save Changes').click()
+		cy.contains('Playlist was successfully updated')
+		cy.contains('Private')
+	})
+})
