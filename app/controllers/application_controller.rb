f97e42f6c365665b99484e599ef47a8c7547c20b
doug-hahn <dhahn@tamu.edu>
2022-07-11 07:56:02 -0500

    Bring Master into alignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -151,7 +151,8 @@ class ApplicationController < ActionController::Base
   def current_ability
     session_opts ||= user_session
     session_opts ||= {}
-    @current_ability ||= Ability.new(current_user, session_opts.merge(remote_ip: request.remote_ip))
+	@current_ability ||= Ability.new(current_user, session_opts.merge(remote_ip: request.env['HTTP_X_REAL_IP']))
+#    @current_ability ||= Ability.new(current_user, session_opts.merge(remote_ip: request.remote_ip))
   end
 
   rescue_from CanCan::AccessDenied do |exception|
