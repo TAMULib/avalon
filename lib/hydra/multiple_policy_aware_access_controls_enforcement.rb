65a9f00d6d25d4d731d8d60b74e7757dc2992d64
doug-hahn <dhahn@tamu.edu>
2022-10-13 04:14:20 -0500

    missing settings

--- lib/hydra/multiple_policy_aware_access_controls_enforcement.rb
+++ lib/hydra/multiple_policy_aware_access_controls_enforcement.rb
@@ -51,7 +51,9 @@ module Hydra::MultiplePolicyAwareAccessControlsEnforcement
       user_access_filters = []
       current_ability.user_groups.each_with_index do |group, i|
         permission_types.each do |type|
-          user_access_filters << "(" + escape_filter(Hydra.config.permissions.inheritable[type.to_sym].group, group) + policy_class_clause + ")"
+			if !group.nil? && !group.empty?
+				user_access_filters << "(" + escape_filter(Hydra.config.permissions.inheritable[type.to_sym].group, group) + policy_class_clause + ")"
+			end
         end
       end
       user_access_filters
