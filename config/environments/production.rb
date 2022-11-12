0c9e527daabea74980e72fc1de92cfde1c7aedb4
doug-hahn <dhahn@tamu.edu>
2022-11-12 06:57:38 -0600

    turn off deprecation notices

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = false
+  config.webpacker.check_yarn_integrity = false
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -105,7 +105,9 @@ config.webpacker.check_yarn_integrity = false
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silence
+  ActiveSupport::Deprecation.silenced = true
+  config.active_support.report_deprecations = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
