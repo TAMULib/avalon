c43450a695c327fdc15b7c482ac02bab3676ffb7
Douglas Hahn <dhahn@tamu.edu>
2025-01-30 09:38:07 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << ENV['env_rails_hosts']
+  
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
